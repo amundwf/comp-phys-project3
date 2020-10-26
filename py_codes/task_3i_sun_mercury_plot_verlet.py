bcc2a46d86ef1f21bd5c0189862e37b8b1932eea
Amund Fredriksen <amund.fredriksen@outlook.com>
2020-10-26 23:57:52 +0100

    Updated task_3i_sun_mercury_plot_verlet.py

--- py_codes/task_3i_sun_mercury_plot_verlet.py
+++ py_codes/task_3i_sun_mercury_plot_verlet.py
@@ -7,14 +7,17 @@ import os
 
 ### Optional: Run the C++ program to get an updated data file. 
 # Compile and run the C++ files (this is exactly what is in the makefile):
+'''
 os.system("echo compiling C++ codes...")
 os.system("g++ -o main.out ../cpp_codes/main.cpp ../cpp_codes/utils.cpp -larmadillo")
 os.system("echo executing...")
 os.system("./main.out")
 ###
-
+'''
 # Read the comma-separated data files (two columns, x and y):
 directory = "../results/3i_mercury_precession/"
+
+############################################################################
 filename = "mercury_sun_verlet.csv"
 #filePath = fullfile(directory, filename)
 filePath = os.path.join(directory, filename) # The full file path.
@@ -36,7 +39,45 @@ plot1, = plt.plot(xList, yList, label='velocityVerlet')
 plt.grid()
 plt.xlabel(r'$x$')
 plt.ylabel(r'$y$')
-plt.xlim(-1, 1)
-plt.ylim(-1, 1)
-plt.suptitle('Mercury-Sun system, velocity Verlet')
+plt.xlim(-0.5, 0.5)
+plt.ylim(-0.5, 0.5)
+plt.suptitle('Mercury-Sun system with relativistic correction')
+plt.savefig(directory + "/mercury-sun.pdf", dpi = "figure")
 plt.show()
+
+#############################################################################
+
+filename = "perihelion.csv"
+filePath = os.path.join(directory, filename) 
+data = np.loadtxt(filePath, skiprows=1, delimiter=",")
+
+# Get the columns as lists:
+data = pd.DataFrame(data, columns=["x", "y", "z"])
+data = data.loc[~(data==0).all(axis=1)]
+
+############################################################################
+fig = plt.figure()
+plt.scatter(data["x"], data["y"], marker = ".", linewidth = 0.1)
+plt.grid()
+plt.xlabel(r'$x$')
+plt.ylabel(r'$y$')
+plt.xlim(data["x"].min(), data["x"].max() )
+plt.ylim(data["y"].min(), data["y"].max() )
+plt.ticklabel_format(axis="both", style="sci", scilimits=(0,0))
+plt.suptitle('Position of the perihelion over a period of a century')
+plt.savefig(directory + "/perihelion.pdf", dpi = "figure")
+plt.show()
+
+#############################################################################
+fig = plt.figure()
+
+theta = np.arctan(data["y"]/data["x"])*360*3600/(2*np.pi)
+plt.grid()
+plt.ylabel("perihelion angle /arc seconds")
+plt.xlabel("revolutions")
+
+x = np.arange(len(theta))
+plt.scatter(x, theta, marker = ".", linewidth = 0.1)
+plt.savefig(directory + "/angle_theta.pdf", dpi = "figure")
+print(data.shape)
+print(theta)
